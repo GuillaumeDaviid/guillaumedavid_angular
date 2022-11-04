f5c4e71a6219dfc146e8d8cb6e3378dee42ee19a
Guillaume David <guillaumedaavidpro@gmail.com>
2022-11-04 15:23:53 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ArticleComponent } from './article/article.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WhyComponent } from './why/why.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ArticleComponent,
+    SkillsComponent,
+    WhyComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
