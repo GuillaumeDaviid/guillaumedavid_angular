09d959d673aa010c29d1ff6af02a9c0ec5f69d88
Guillaume David <guillaumedaavidpro@gmail.com>
2022-11-08 10:09:53 +0100

    add data to portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Portfolio} from "../models/portfolio-models"
 
 @Component({
   selector: 'app-portfolio',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  portfolio!:Portfolio[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.portfolio = [
+      {
+        id: 1,
+        title: "01 Academie",
+        link: "https://github.com/GuillaumeDaviid/01-Academie-Version-React",
+        image: "../../images/01academie.png"
+      },
+      {
+        id: 2,
+        title: "Netflix",
+        link: "https://clone-guillaumedavid.herokuapp.com/",
+        image: "../../images/netflix.png"
+      },
+      {
+        id: 3,
+        title: "Commercia",
+        link: "https://commercia.vercel.app/",
+        image: "../../images/commercia.png"
+      },
+      {
+        id: 4,
+        title: "Jeu de Dame",
+        link: "https://guillaumedaviid.github.io/Checkers-Game/",
+        image: "../../images/checkers.png"
+      },
+      {
+        id: 5,
+        title: "Leagora",
+        link: "https://visio.leagora.fr/",
+        image: "../../images/leagora.png"
+      },
+    ]
   }
 
 }
