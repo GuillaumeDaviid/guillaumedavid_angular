cb713647aa2bd7a7b2f79e497e091bae2df52813
Guillaume David <guillaumedaavidpro@gmail.com>
2022-11-14 15:10:35 +0100

    add form style + submit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.form = this.formBuilder.group({
+        name: [null],
+        email: [null],
+        message: [null],
+    });
+}
+
+onSubmitForm() {
+  console.log(this.form.value);
+}
 
 }
