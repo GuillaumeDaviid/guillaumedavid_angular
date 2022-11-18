8b89569bbb5aada74e86ccc69171f6ff086761e2
Guillaume David <guillaumedaavidpro@gmail.com>
2022-11-18 15:33:04 +0100

    ts: add handleClickFunction

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -11,7 +11,9 @@ import { ArticleService } from "../services/articles.service"
 export class ArticleComponent implements OnInit {
 
   article$!: Observable<Articles>
-  data:any=[]
+  data:any=[];
+  nbr: number = 1;
+  p: number = 0;
 
   constructor(private articleService: ArticleService) {
     this.articleService.getAllArticles().subscribe(data => {
@@ -22,8 +24,30 @@ export class ArticleComponent implements OnInit {
 
   ngOnInit(): void {
     this.article$ = this.articleService.getAllArticles()
-   /* this.article$.subscribe(data => {
-      console.warn(data)
-    })*/
   }
+
+   handleClickNext() {
+    let carrousel = document.getElementById('carrousel');
+    if (carrousel){
+        if (this.nbr < 8){
+            this.nbr ++;
+            this.p --;
+            carrousel.style.transform = "translate("+this.p*25+"vw)";
+            carrousel.style.transition = "all 0.5s ease";
+        }
+    }
+   }
+
+     handleClickPrevious() {
+        let carrousel = document.getElementById('carrousel');
+        if (carrousel){
+      if (this.nbr > 1){
+        this.nbr --;
+        this.p ++;
+      carrousel.style.transform = "translate("+this.p*25+"vw)";
+      carrousel.style.transition = "all 0.5s ease";
+      }
+      
+        }
+    }
 }
